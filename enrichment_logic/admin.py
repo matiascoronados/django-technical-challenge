b06c2a1f70a00cf364dc60e057b9ec7f8f8c723f
Matias Coronado <mcoronado1995@gmail.com>
2025-04-26 15:03:35 -0400

    Registro de modelos
    Se registran los modelos en la base de datos por defecto (sqlite3)

--- enrichment_logic/admin.py
+++ enrichment_logic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Merchant, Keyword, Transaction
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Merchant)
+admin.site.register(Keyword)
+admin.site.register(Transaction)
