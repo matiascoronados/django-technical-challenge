0f93f02ea6c22f2605f9d55adc366eb6b8c81fd1
Matias Coronado <mcoronado1995@gmail.com>
2025-04-28 00:26:26 -0400

    Cambio a Swagger
    Se cambia para utilzar el DRF Swagger para la documentación de la API

--- enrichment_project/settings.py
+++ enrichment_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'enrichment_logic',
     'rest_framework',
-    'coreapi'
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
