f3a528912a4ab6157dc66878663d555c864e9f14
Matias Coronado <mcoronado1995@gmail.com>
2025-04-28 00:28:11 -0400

    Modificación de Serializers
    Se especifican cuales son los campos que son solo de lectura para los serializers de los modelos. Y ademas, se agregan serializers relacionados al manejo de las transacciones.

--- enrichment_logic/serializer.py
+++ enrichment_logic/serializer.py
@@ -1,22 +1,36 @@
 from rest_framework import serializers
-from .models import Category, Merchant, Keyword, Transaction
+from .models import Category, Merchant, Keyword
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ('id', 'name', 'type', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
         
 class MerchantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Merchant
-        fields = '__all__'
+        fields = ('id', 'merchant_name', 'merchant_logo', 'category', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
         
 class KeywordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
-        fields = '__all__'
-        
-class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = '__all__'
+        fields = ('id', 'keyword', 'merchant', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
+
+class InputTransactionSerializer(serializers.Serializer):
+    description = serializers.CharField(required=True)
+    amount = serializers.DecimalField(required=True, max_digits=10, decimal_places=2)
+    date = serializers.DateField(required=True)
+    
+class OutputTransactionSerializer(serializers.Serializer):
+    description = serializers.CharField(read_only=True)
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    date = serializers.DateField(read_only=True)
+    enriched_category = CategorySerializer(read_only=True, allow_null=True)
+    enriched_merchant = MerchantSerializer(read_only=True, allow_null=True)
+
+class EnrichmentResponseSerializer(serializers.Serializer):
+    transactions = OutputTransactionSerializer(many=True, read_only=True)
+    metrics = serializers.DictField(read_only=True)
